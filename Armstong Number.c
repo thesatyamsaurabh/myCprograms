08de6f979a0b3d1e62868a890c9f7f26e3204e0b
thesatyamsaurabh <133398896+thesatyamsaurabh@users.noreply.github.com>
2023-05-12 22:28:43 +0530

    Add files via upload

---
+++ Armstong Number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+int r,n,s=0,x,count=0,i,k,p;
+printf("Enter the number you want to check for Armstrong:");
+scanf("%d",&n);
+x=n;
+p=n;
+while(n>0)
+{
+r=n%10;
+count++;
+n=n/10;
+}
+printf("It is a %d digit number\n",count);
+for(i=1;i<=count;i++)
+{
+ k=p%10;
+ s=s+pow(k,count);
+ p=p/10;
+}
+if(s==x)
+printf("Armstrong Number");
+else
+printf("Not Armstrong Number");
+getch();
+}
